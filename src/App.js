57ce0c3433d9c393ea7e1ed2ce02c4083533cfb4
Martin Young <martingyoung@hotmail.com>
2021-03-07 11:46:42 -0500

    updating our app with a shop page, collection preview component and collection item component

--- src/App.js
+++ src/App.js
@@ -1,38 +1,21 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import './App.css';
-import HomePage from './pages/homepage/homepage.component'
+import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
 
-const HatsPage = () => (
+const HatsPage = () => {
   <div>
     <h1>HATS PAGE</h1>
   </div>
-)
-
-// const TopicsList = () => {
-//   return (
-//     <div>
-//       <h1>TOPIC LIST PAGE</h1>
-//     </div>
-//   );
-// }
-
-// const TopicDetail = () => {
-//   return (
-//     <div>
-//       <h1>TOPIC DETAIL</h1>
-//     </div>
-//   )
-// }
+}
 
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path='/' component={HomePage} />
-        <Route path='/hats' component={HatsPage} />        
-        {/* <Route exact path='/topics' component={TopicsList} />
-        <Route path='/:topicId' component={TopicDetail} /> */}
+        <Route path='/shop' component={ShopPage} />        
       </Switch>
     </div>
   );
